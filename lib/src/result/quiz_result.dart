760f7064e1024ffcccb49eb8315a5250e8d59402
izasoerya <ihza.surya@gmail.com>
2024-01-05 12:45:51 +0700

    change: name file

---
+++ lib/src/result/quiz_result.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class QuizResult extends StatelessWidget {
+  const QuizResult({Key? key, required this.resultQuiz}) : super(key: key);
+  final List<Map<String, Object>> resultQuiz;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+            children: resultQuiz.map((data) {
+          return Row(
+            children: [
+              const SizedBox(
+                height: 30,
+              ),
+              Text(((data['index'] as int) + 1).toString()),
+              Expanded(
+                child: Column(
+                  children: [
+                    Text(data['question'] as String),
+                    Text(data['answer'] as String),
+                    Text(data['correct_answer'] as String),
+                  ],
+                ),
+              )
+            ],
+          );
+        }).toList()),
+      ),
+    );
+  }
+}
